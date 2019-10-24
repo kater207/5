ed8b430b3d9da32a61689d0285ae65e52e3876f5
Екатерина <tkachenkokaterina@gmail.com>
2019-10-24 15:05:02 +0300

    First commit1

---
+++ src/com/company/Dog.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public interface Dog extends DogGuide{
+    String name = "Pes";
+    boolean isVaccinated = true;
+    boolean isTrained = true;
+
+    void voiceOfDog();
+
+
+}
